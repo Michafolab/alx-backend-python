cdfa2c5003dc61e94a401d72d3121a56a595999b
Michafolab <TOPSONMICH1@GMAIL.COM>
2024-01-12 22:02:19 +0000

    task

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Annotation Function"""
+"""Annotation function"""
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """returns a tuple"""
-    return k, v * 2
+    """return tuple"""
+    return k, v ** 2
