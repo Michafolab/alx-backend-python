ea4627aae769bd9a50b52832ba1c3dfa3e91cc1a
Michafolab <TOPSONMICH1@GMAIL.COM>
2024-01-16 01:25:30 +0000

    task

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,6 +8,6 @@ import random
 async def wait_random(max_delay: int = 10) -> float:
     """doc
     """
-    delay = random.uniform(n, max_delay)
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
