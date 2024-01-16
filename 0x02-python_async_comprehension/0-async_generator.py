44cd0a773aaeff01303dabdfa3960038ee4cec4c
Michafolab <TOPSONMICH1@GMAIL.COM>
2024-01-16 22:41:06 +0000

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""asyncio generator"""
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """doc
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
