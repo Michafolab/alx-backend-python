4aa4865ba4a42a35bdce45b1da81d58dc3cd456d
Michafolab <TOPSONMICH1@GMAIL.COM>
2024-01-16 01:39:47 +0000

    task

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-""" """
+""" Module documentation """
 import asyncio
+import random
 from typing import List
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """doc
-    """
-    list_delay = [asyncio.create_task(wait_random(max_delay))
-                  for _ in range(n)]
-    return sorted(await asyncio.gather(*list_delay))
+    """doc func"""
+    listDelays = []
+    for _ in range(n):
+        listDelays.append(asyncio.create_task(wait_random(max_delay)))
+    return sorted(await asyncio.gather(*listDelays))
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-'''
-Test file for printing the correct output of the wait_n coroutine
-'''
+""" Module documentation """
 import asyncio
 import time
 
-wait_n = __import__('1-concurrent_coroutines').wait_n
+wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """doc
-    """
-    start = time.pref_counter()
+    """doc func"""
+    start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    end = time.pref_counter() - start
-    return end / n
+    end = time.perf_counter()
+    return (end - start) / n
